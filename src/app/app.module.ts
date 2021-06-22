6c339f258f3ffe43763734b6ac738745e2fee0d4
JMSobrinos <libreyextremo@gmail.com>
2021-06-22 12:16:52 +0200

    data binding problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+//  Activate only the one that is going to be used
+// import localeAr from '@angular/common/locales/ar';
+// import localeBg from '@angular/common/locales/bg';
+// import localeDe from '@angular/common/locales/de';
+import localeEn from '@angular/common/locales/en';
+// import localeEs from '@angular/common/locales/es';
+// import localeFr from '@angular/common/locales/fr';
+// import localeLt from '@angular/common/locales/lt';
+// import localeNb from '@angular/common/locales/nb';
+// import localePt from '@angular/common/locales/pt';
+// import localeSv from '@angular/common/locales/sv';
+
+registerLocaleData(localeEn);
 
 @NgModule({
   declarations: [
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
-import { LOCALE_ID, Inject } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeAr from '@angular/common/locales/ar';
+import localeBg from '@angular/common/locales/bg';
+import localeDe from '@angular/common/locales/de';
+import localeEn from '@angular/common/locales/en';
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+import localeLt from '@angular/common/locales/lt';
+import localeNb from '@angular/common/locales/nb';
+import localePt from '@angular/common/locales/pt';
+import localeSv from '@angular/common/locales/sv';
 
 @Component({
   selector: 'app-nav',
@@ -20,6 +30,7 @@ export class NavComponent implements OnInit {
    */
   constructor() {
     this.localeSetting = localStorage.getItem('lang') || 'en-US';
+    this.setRegisterLocaleData();
     this.setCurrencyCode();
   }
 
@@ -29,8 +40,110 @@ export class NavComponent implements OnInit {
    * @return textfield value without currency symbol (34,44)
    */
   currencyInputChanged(value: any): any {
-    const num = value.replace(/[A-Z$€,]/g, '');
-    return Number(num);
+    let num;
+    switch (this.localeSetting) {
+      case 'ar-MA':
+        // 9,876,543,210.99 د.م => 9876543210.99
+        num = value.replace(/[€\s,]/g, ''); // \s => replace white spaces
+        num = num.replace(/[د.م]/g, ''); // currency symbol
+        return Number(num);
+      case 'bg-BG':
+        // 9876543210,99 € => 9876543210.99
+        num = value.replace(/[€\s]/g, ''); // \s => replace white spaces
+        num = num.replace(/,/g, '.'); // replace , for .
+        return Number(num);
+      case 'de-DE':
+        // 9.876.543.210,99 € => 9876543210.99
+        num = value.replace(/[€\s.]/g, ''); // \s => replace white spaces
+        num = num.replace(/,/g, '.'); // replace , for .
+        return Number(num);
+      case 'en-US':
+        // $9,876,543,210.99 => 9876543210.99
+        num = value.replace(/[$€,]/g, '');
+        return Number(num);
+      case 'es-CO':
+        // 9.876.543.210,99 COP => 9876543210.99
+        num = value.replace(/[A-Z\s.]/g, ''); // \s => replace white spaces
+        num = num.replace(/,/g, '.'); // replace , for .
+        return Number(num);
+      case 'es-ES':
+        // 9.876.543.210,99 € => 9876543210.99
+        num = value.replace(/[€\s.]/g, ''); // \s => replace white spaces
+        num = num.replace(/,/g, '.'); // replace , for .
+        return Number(num);
+      case 'fr-FR':
+        // 9 876 543 210,99 € => 9876543210.99
+        num = value.replace(/[€\s]/g, '');
+        num = num.replace(/,/g, '.'); // replace , for .
+        return Number(num);
+      case 'lt-LT':
+        // 9 876 543 210,99 € => 9876543210.99
+        num = value.replace(/[€\s]/g, '');
+        num = num.replace(/,/g, '.'); // replace , for .
+        return Number(num);
+      case 'nb-NO':
+        // kr 9 876 543 210,99 => 9876543210.99
+        num = value.replace(/[a-zA-Z\s]/g, '');
+        num = num.replace(/,/g, '.'); // replace , for .
+        return Number(num);
+      case 'pt-PT':
+        // € 9.876.543.210,99 => 9876543210.99
+        num = value.replace(/[€\s.]/g, '');
+        num = num.replace(/,/g, '.'); // replace , for .
+        return Number(num);
+      case 'sv-SE':
+        // 9 876 543 210,99 kr => 9876543210.99
+        num = value.replace(/[a-zA-Z$€,]/g, '');
+        num = num.replace(/,/g, '.'); // replace , for .
+        return Number(num);
+      default:
+        // Display error message because in other case, database will receive a wrong value
+        alert('ERROR: this currency format is not supported');
+    }
+  }
+
+  /**
+   * Set locale culture
+   */
+  setRegisterLocaleData(): void {
+    switch (this.localeSetting) {
+      case 'ar-MA':
+        registerLocaleData(localeAr);
+        break;
+      case 'bg-BG':
+        registerLocaleData(localeBg);
+        break;
+      case 'de-DE':
+        registerLocaleData(localeDe);
+        break;
+      case 'en-US':
+        registerLocaleData(localeEn);
+        break;
+      case 'es-CO':
+        registerLocaleData(localeEs);
+        break;
+      case 'es-ES':
+        registerLocaleData(localeEs);
+        break;
+      case 'fr-FR':
+        registerLocaleData(localeFr);
+        break;
+      case 'lt-LT':
+        registerLocaleData(localeLt);
+        break;
+      case 'nb-NO':
+        registerLocaleData(localeNb);
+        break;
+      case 'pt-PT':
+        registerLocaleData(localePt);
+        break;
+      case 'sv-SE':
+        registerLocaleData(localeSv);
+        break;
+      default:
+        registerLocaleData(localeEn);
+        break;
+    }
   }
 
   /**
@@ -76,6 +189,7 @@ export class NavComponent implements OnInit {
         break;
     }
   }
+
   ngOnInit(): void {}
 
   /**
@@ -83,9 +197,9 @@ export class NavComponent implements OnInit {
    * @param lang language selected (es-ES).
    */
   changeLang(lang): void{
-    console.log(lang);
     localStorage.setItem('lang', lang);
     this.localeSetting = lang;
+    this.setRegisterLocaleData();
     this.setCurrencyCode();
     window.location.reload();
   }
